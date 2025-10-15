094c180be7a33aff12a209563002131012923310
nulledus <sfuhsjfsfkje@srfsfkssese>
2025-10-15 17:19:26 +0300

    clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { WellKnownModule } from './modules/well-known/well-known.module';
+import { PingModule } from './modules/ping/ping.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { NetworksModule } from './modules/networks/networks.module';
-import { PingModule } from './modules/ping/ping.module';
-import { WellKnownModule } from './modules/well-known/well-known.module';
 
 @Module({
   imports: [
